716ce2f7323372aa41988342facb48b12d3226e7
KIRANMAYI RAVURI <74241589+kiranwhitehatjr@users.noreply.github.com>
2021-08-14 15:04:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+    function cal()
+{
+    
+    var l= Number(document.getElementById("side").value);
+    document.getElementById("area").innerHTML=(l*l);
+    document.getElementById("peri").innerHTML=(4*l);
+}
